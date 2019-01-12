5e1b0153883d39fb10fceba55c5605c852806cdc
CAIMEOX <38813005+CAIMEOX@users.noreply.github.com>
2019-01-12 11:53:27 +0800

    Update Algorithms.js

--- Algorithms.js
+++ Algorithms.js
@@ -1,13 +1,3 @@
-function clear(array){
-  var n = []; //一个新的临时数组
-  //遍历当前数组
-  for(var i = 0; i < array.length; i++){
-    //如果当前数组的第i已经保存进了临时数组，那么跳过，
-    //否则把当前项push到临时数组里面
-    if (n.indexOf(array[i]) == -1) n.push(array[i]);
-  }
-  return n;
-}
 module.exports = {
 ligature(PosArray1, PosArray2){
     var session = new Array();
