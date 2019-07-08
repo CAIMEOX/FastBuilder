e428b81737d50a0fdd46fbf400293281ff0cc3de
CAIMEOX <1853884864@qq.com>
2019-07-08 21:25:50 +0800

    全新版本 	修改：     Main.js

--- Main.js
+++ Main.js
@@ -1,32 +1,54 @@
-const colorize = require('./core/colorize');
-const WSServer = require('./core/wsserver');
-const BuildSession = require('./core/session');
-const profile = require('./core/profile');
-const os = require('os');
+const MeoWS = require('meowslib');
+const WSServer = MeoWS.WSServer;
+const BuildSession = MeoWS.BuildSession;
+const Collector = MeoWS.Collector;
+const Commander = MeoWS.Commander;
+const Builder = MeoWS.Builder;
+const Parser = MeoWS.Parser;
+const Lang = MeoWS.Res.Lang;
+const log = MeoWS.Logger;
 
-let localhost;
-if(os.type == 'Linux'){
-  try{
-    localhost = os.networkInterfaces()[Object.keys(os.networkInterfaces())[1]][0].address + ':8080';
-  }catch (e) {
-    localhost = '127.0.0.1:8080';
-  }
-}else{
-  try{
-    localhost = os.networkInterfaces()[Object.keys(os.networkInterfaces())[0]][1].address + ':8080';
-  }catch (e) {
-    localhost = '127.0.0.1:8080';
-  }
-}
-let wss = new WSServer(8080);
-console.log(colorize(profile.server + localhost).yellow);
-console.log(colorize('FastBuilder Parsley by CAIMEO.').yellow);
-console.log(colorize(profile.logo).yellow);
-wss.on('client', function(session, request) {
-  BuildSession.createAndBind(session);
-  console.log(colorize(request.connection.remoteAddress + profile.connected).blue);
+let wss = new WSServer(16384);
+
+console.log('\x1b[33m' + Lang.logo)
+wss.on('client', (session, request) => {
+//init
+	this.CMD = new Commander(session);
+	this.C = new Collector(session);
+	this.B = new Builder(session, this.C);
+
+	log(request.connection.remoteAddress + Lang.connected);
+	session.tellraw(Lang.client_ctd);
+	BuildSession.createAndBind(session);
+	this.C.getPosition().then((v)=>{
+		session.tellraw('Position got: ' + v.join(' '));
+	});
+
+	
+	session.sendCommandSync('testfor @s').then((v) => {
+		session.tellraw(v.victim + Lang.welcome);
+		session.write('§ePowered by CAIMEO.');
+	});
+
+
+	session.on('onMessage',(msg)=>{
+		let config = new Parser(msg,{},Object.keys(this.B.methods));
+		let exec = false;
+		for(let c of config.config){
+			if(c.exec)exec = true;
+		}
+		if(!exec)return;
+		let result = this.CMD.ParseConfig(config,this.B,this.C.config);
+		if(!result.queue){
+			return;
+		}
+		if(result.sync){
+			session.sendCommandQueueSync(result.queue,0,true);
+		}else{
+			session.sendCommandQueue(result.queue,0,true);
+		}
+
+	});
+
+
 });
-
-
-//When I wrote this, only God and I understood what I was doing.
-//Now, God only knows.
--- script/main.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-    {
-        name:'paint',
-        bin:console.log('BINARY')
-    }
-]
