6e4bf3df851927cef20c218eef8e363c56cc14ba
CAIMEO <38813005+CAIMEOX@users.noreply.github.com>
2020-04-08 23:04:00 +0800

    FastBuilder V8

--- Main.js
+++ Main.js
@@ -1,53 +1,55 @@
 const MeoWS = require('meowslib');
 const WSServer = MeoWS.WSServer;
+const log = MeoWS.Logger;
 const BuildSession = MeoWS.BuildSession;
 const Collector = MeoWS.Collector;
 const Commander = MeoWS.Commander;
 const Builder = MeoWS.Builder;
 const Parser = MeoWS.Parser;
-const Lang = MeoWS.Res.Lang;
-const log = MeoWS.Logger;
-
+const fs = require('fs');
+let logo = "\n" +
+"    ______           __  ____        _ __    __          \n" +
+"   / ____/___ ______/ /_/ __ )__  __(_) /___/ /__  _____ \n" +
+"  / /_  / __ `/ ___/ __/ __  / / / / / / __  / _ \\/ ___/\n" +
+" / __/ / /_/ (__  ) /_/ /_/ / /_/ / / / /_/ /  __/ /     \n" +
+"/_/    \\__,_/____/\\__/_____/\\__,_/_/_/\\__,_/\\___/_/ \n" +
+"                                                         \n";
 let wss = new WSServer(16384);
-
-console.log('\x1b[33m' + Lang.logo)
-wss.on('client', (session, request) => {
+log(logo);
+wss.on('client', async (session, request) => {
+	await new Promise((r)=>{setTimeout(r,1000)});
 //init
-	this.CMD = new Commander(session);
-	this.C = new Collector(session);
-	this.B = new Builder(session, this.C);
-
-	log(request.connection.remoteAddress + Lang.connected);
-	session.tellraw(Lang.client_ctd);
+	session.cmd = new Commander({});
+	session.cmd.updateOptions({
+		block:'iron_block',
+		data:0
+	})
+	session.clr = new Collector(session);
+	session.gen = new Builder(session);
+	session.parser = new Parser(Object.keys(session.gen.methods));
+	log(request.connection.remoteAddress + ' connected!');
+	session.tellraw(session.now() + 'FastBuilder connected!');
 	BuildSession.createAndBind(session);
-	this.C.getPosition().then((v)=>{
-		session.tellraw('Position got: ' + v.join(' '));
+	session.clr.getPosition().then((v)=>{
+		session.cmd.updateOptions({position:v});
+		session.tellraw(session.now() + 'Position get: ' + v.join(' '));
 	});
-
-	
 	session.sendCommandSync('testfor @s').then((v) => {
-		session.tellraw(v.victim + Lang.welcome);
+		session.tellraw(session.now() + v.victim + '. Welcome to FastBuilder! Enjoy it:D');
 		session.write('§ePowered by CAIMEO.');
 	});
 
-
 	session.on('onMessage',(msg)=>{
-		let config = new Parser(msg,{},Object.keys(this.B.methods));
-		let exec = false;
-		for(let c of config.config){
-			if(c.exec)exec = true;
-		}
-		if(!exec)return;
-		let result = this.CMD.ParseConfig(config,this.B,this.C.config);
-		if(!result.queue){
+		let result = session.cmd.parseConfigs(session.parser.parsePipe(msg),session.gen.methods);
+		if(!result){
 			return;
 		}
-		if(result.sync){
-			session.sendCommandQueueSync(result.queue,0,true);
-		}else{
-			session.sendCommandQueue(result.queue,0,true);
+		if (result.err){
+			if (result.err === 'No exec') return;
+			session.tellraw(result.err);
+			return
 		}
-
+		session.sendCommandQueue(result, 0, true);
 	});
 
 
